66b662984850a3576d787a5efce2bb9ffb3ad7b7
Gaston Negrete De La Cruz <gastonnegrete@gmail.com>
2022-07-11 12:09:49 -0300

    Creacion de clase Alumno con metodo para calcular promedio. Modificacion de funcion de registro para interaccion con array de objetos

--- simulador-interactivo/script.js
+++ simulador-interactivo/script.js
@@ -10,39 +10,67 @@ const validadorNota = function(){
     return nota
 }
 
-// Función de registro que muestra en pantalla toda la info de notas del alumno, asi como su conidición final
+// Clase alumno:
 
-const infoCondicionALumno = function(alumno, nota1, nota2, nota3, promedio){
-    console.log(`--------REGISTRO DE NOTAS DEL ALUMNO: ${alumno}--------`)
-    console.log(`NOTA 1: ${nota1}`)
-    console.log(`NOTA 2: ${nota2}`)
-    console.log(`NOTA 3: ${nota3}`)
+class Alumno{
+    constructor(id, nombre, nota1, nota2, nota3){
+        this.id = id
+        this.nombre = nombre;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
 
-    if (promedio < 4){
-        console.log(`LA NOTA FINAL PROMEDIO DEL ALUMNO ES : ${promedio}`)
-        console.log(`CONDICIÓN FINAL : LIBRE`)
-    } else if(promedio < 8){
-        console.log(`LA NOTA FINAL PROMEDIO DEL ALUMNO ES : ${promedio}`)
-        console.log(`CONDICIÓN FINAL : REGULAR`)
-    } else if(promedio <= 10){
-        console.log(`LA NOTA FINAL PROMEDIO DEL ALUMNO ES : ${promedio}`)
-        console.log(`CONDICIÓN FINAL : PROMOVIDO`)
-    } else{console.log(`DEBE INGRESAR NOTAS VÁLIDAS...`)}
+    // Metodo para calcular promedio
+    promedio(){
+        let notaFinal = (this.nota1 + this.nota2 + this.nota3) / 3;
+        return notaFinal
+    }
 }
 
-// Pedido de nombre del alumno para registro:
-let nombreAlumno = prompt('Ingrese el nombre del alumno: ')
+// Función de registro que muestra en pantalla toda la info de notas de los alumnos, asi como su conidición final
 
-//Llamada a funcion para pedido de notas y validación de las mismas:
-let nota1 = validadorNota()
-let nota2 = validadorNota()
-let nota3 = validadorNota()
+const infoCondicionALumno = function(arrayAlumnos){
+    for(Alumno of arrayAlumnos){
+        console.log('--------------------------------------------------------------------')
+        console.log(`--------REGISTRO DE NOTAS DEL ALUMNO: ${Alumno.nombre}--------`)
+        console.log(`ID: ${Alumno.id}`)
+        console.log(`NOTA 1: ${Alumno.nota1}`)
+        console.log(`NOTA 2: ${Alumno.nota2}`)
+        console.log(`NOTA 3: ${Alumno.nota3}`)
 
-//Calculo de promedio:
-let promedio = (nota1 + nota2 + nota3) / 3
+        let promedio = Alumno.promedio()
+    
+        if (promedio < 4){
+            console.log(`LA NOTA FINAL PROMEDIO DEL ALUMNO ES : ${promedio}`)
+            console.log(`CONDICIÓN FINAL : LIBRE`)
+        } else if(promedio < 8){
+            console.log(`LA NOTA FINAL PROMEDIO DEL ALUMNO ES : ${promedio}`)
+            console.log(`CONDICIÓN FINAL : REGULAR`)
+        } else if(promedio <= 10){
+            console.log(`LA NOTA FINAL PROMEDIO DEL ALUMNO ES : ${promedio}`)
+            console.log(`CONDICIÓN FINAL : PROMOVIDO`)
+        } else{console.log(`DEBE INGRESAR NOTAS VÁLIDAS...`)}
+    }
+
+}
+
+// Creacion de array de alumnos:
+
+let alumnosPrimerAnio = []
+
+let cantidadAlumnos = parseInt(prompt('Ingrese la cantidad de alumnos que desea cargar: '))
+
+
+// Ciclo a utilizar para crear objeto alumnos segun la cantidad informada por el usuario:
+for(i = 0; i < cantidadAlumnos; i++){
+    let alumno = new Alumno((prompt('Ingrese el numero de ID del alumno: ')), (prompt('Ingrese el nombre del alumno: ')), parseInt(validadorNota()), parseInt(validadorNota()), parseInt(validadorNota()))
+    alumnosPrimerAnio.push(alumno)
+}
+
 
 //Llamada a función para conocer condición del alumno:
-infoCondicionALumno(nombreAlumno, nota1, nota2, nota3, promedio)
+infoCondicionALumno(alumnosPrimerAnio)
 
 
 
